9e182eca2a3121e3cddc6fd4dad74f13c6fdbd3a
Alex Charrier <alex.charrier@datadoghq.com>
2020-06-03 22:14:27 +0200

    fix pip 20

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@
 """The setup script."""
 
 from setuptools import setup
-try:  # For pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # For pip <= 9
-    from pip.req import parse_requirements
-
+import os
 
 __version__ = '0.1.1'  # Should match with __init.py__
 _PACKAGE_NAME = 'trainline'
@@ -18,8 +14,10 @@ _KEYWORDS = ['api', 'trainline', 'parsing', 'train', 'sncf',
 _SCRIPTS = ['trainline_cli.py']
 _PACKAGE_DATA = ['stations_mini.csv']
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
-requirements = [str(ir.req) for ir in install_reqs]
+HERE = os.path.abspath(os.path.dirname(__file__))
+reqs_path = HERE + "/requirements.txt"
+with open(reqs_path) as reqs_file:
+    requirements = reqs_file.read().splitlines()
 
 setup(
     name=_PACKAGE_NAME,
